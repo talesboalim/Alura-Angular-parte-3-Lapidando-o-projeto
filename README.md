cc1ac869630f123c4e7c241da77d1713cf28748f
talesboalim <talesboalim@gmail.com>
2020-11-19 23:53:08 -0300

    Projeto inicial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Curso Alura - Angular parte 4: Lapidando o projeto
+
+## Instalação inicial
+```
+./api/npm install
+./api/npm install --unsafe-perm -g sqlite3
+```
+
+## Execução
+Node > 8
+Angular 6
+
+```
+./api/npm start
+./alurapic/ng serve --open
+```
---
+++ alurapic/src/app/photos/photos.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PhotoDetailsModule } from './photo-details/photo-details.module';
+
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoModule } from './photo/photo.module';
+
+
+@NgModule({
+    imports: [
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
+        PhotoDetailsModule
+    ]
+})
+export class PhotosModule { }
